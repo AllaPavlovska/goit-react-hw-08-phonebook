16e2eab189b9d3742602f5fb672425e613c31b04
AllaPavlovska <pavlovskaa87@gmail.com>
2024-01-27 14:30:56 +0200

    second-commit

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,19 +3,19 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://65b37c15770d43aba479dab4.mockapi.io';
 
 export const getContacts = async () => {
-  const contacts = await axios.get('/API/contacts');
+  const contacts = await axios.get('/contacts/contacts');
 
   return contacts;
 };
 
 export const getContactById = async contactId => {
-  const contacts = await axios.get(`/API/contacts/${contactId}`);
+  const contacts = await axios.get(`/contacts/contacts/${contactId}`);
 
   return contacts;
 };
 
 export const postContact = async ({ name, phone,  }) => {
-  const contact = await axios.post('/API/contacts', {
+  const contact = await axios.post('/contacts/contacts', {
     name,
     phone,
   });
@@ -24,7 +24,7 @@ export const postContact = async ({ name, phone,  }) => {
 };
 
 export const delContact = async contactId => {
-  const contact = await axios.delete(`/API/contacts/${contactId}`);
+  const contact = await axios.delete(`/contacts/contacts/${contactId}`);
 
   return contact;
 };
